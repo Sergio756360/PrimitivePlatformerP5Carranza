086a6e846d9f7b43c8a15e919691321d0f8dc4ee
Sergio756360 <756360@student.dusd.net>
2026-08-21 13:42:53 -0700

    Input Change
    edited Unity to have both old and new Input manager

--- PrimitivePlatformerCarranzaP5/Assets/Scripts/PlayerMovement.cs
+++ PrimitivePlatformerCarranzaP5/Assets/Scripts/PlayerMovement.cs
@@ -1,7 +1,17 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    public float forwardInput;
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +21,29 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //This is where we get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        //Moves the car forward based on vertical point
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //Rotates the car based on horizontal
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+        // Axis setup
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        //Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        //Rotate the Vehicle left and right
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
     }
 }
